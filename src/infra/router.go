74e8c22dfb9bd8f6a6455a506ec94f8cbab4a557
hisho.suzuki <hisho.suzuki@optim.co.jp>
2025-05-11 16:35:05 +0900

    CROSでTokenヘッダーを許可   ブラウザからdeleteメソッドを叩けなかったから

--- src/infra/router.go
+++ src/infra/router.go
@@ -16,7 +16,7 @@ func NewRouter() *gin.Engine {
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"}, /*TODO: deploy時、変更必須箇所*/
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Token"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
