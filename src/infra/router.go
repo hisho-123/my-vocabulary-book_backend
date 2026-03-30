01c035edafa54d39a65531b8584e6ffd9a24d69a
hisho-123 <vahcp92774@yahoo.co.jp>
2026-03-30 15:38:46 +0900

    deploy後の修正
    DBのテーブル作成を自動デプロイに追加
    アプリポートを80に変更

--- src/infra/router.go
+++ src/infra/router.go
@@ -14,7 +14,7 @@ func NewRouter() *gin.Engine {
 
 	// CORSの設定
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, /*TODO: deploy時、変更必須箇所*/
+		AllowOrigins:     []string{"http://localhost:5173"}, /*本番は同一オリジンのため不要。ローカル開発用*/
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Token"},
 		ExposeHeaders:    []string{"Content-Length"},
